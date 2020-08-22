60e6d54f75353624030ddf44b58939f981a9c100
Syed Mohammad Hassan <hassan.imroze@hotmail.com>
2020-08-22 17:37:00 +0600

    Add project files.

---
+++ BooklistRazor/Pages/BookList/Index.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BooklistRazor.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace BooklistRazor.Pages.BookList
+{
+    public class IndexModel : PageModel
+    {
+        #region declaration & constructor
+        private readonly ApplicationDbContext _db;
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        #endregion
+
+        #region Properties
+
+        public List<Book> Books { get; set; }
+
+        #endregion
+
+        #region Handler Functions
+        public async Task OnGet()
+        {
+            Books = await _db.Books.ToListAsync();
+        } 
+
+        #endregion
+    }
+}
